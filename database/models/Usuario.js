368db7821a0bbb97dd053d3fff0042d36b2cbd12
Mariela Vallecillo <mvalletelt@gmail.com>
2020-08-03 09:37:40 -0300

    agrego los modelos sequelize

---
+++ database/models/Usuario.js
@@ -0,0 +1,60 @@
+module.exports = (sequelize, dataTypes) =>{
+    let alias ='Usuario';
+  
+    let cols={
+  
+   
+          id:{
+  
+              primaryKey:true,
+              autoIncrement:true,
+              allowNull:false,
+              type:dataTypes.INTEGER
+  
+              },
+           nombre:{
+              type:dataTypes.STRING(100)
+  
+            },          
+  
+             apellido:{
+              
+              type:dataTypes.STRING(100)
+              },
+  
+            
+             email:{
+             
+                type:dataTypes.STRING(100)
+             },
+          
+           
+
+            contrasenia: {
+              type:dataTypes.STRING(300)
+          },
+  
+           categoria: {
+              type:dataTypes.STRING(200)
+  
+            },
+             
+           
+           imagen: {
+              type:dataTypes.STRING(100)
+  
+            },
+      
+    }
+      let config= {
+          tableName: "usuarios",
+          timestamps: false
+      }
+      
+      const Usuario = sequelize.define(alias,cols,config);
+          
+      
+  
+      return Usuario;
+  
+  }
